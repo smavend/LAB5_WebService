25799920a25ebe9e00490261e19b3e449ce4031f
Gustavo <a20192858@pucp.edu.pe>
2023-10-23 11:41:57 -0500

    webservice_v3

--- nodeConnection.js
+++ nodeConnection.js
@@ -10,20 +10,70 @@ const connection = mysql.createConnection({
 });
  // Definir la ruta para consultar empleados por managerId
  app.get('/trabajadores/:managerId', (req, res) => {
-    console.log('Receiving a request at /trabajadores/:managerId');
-    const managerId = req.params.managerId;
-    const query = `SELECT * FROM hr_v2.employees WHERE manager_id = ?`;
+  console.log('Receiving a request at /trabajadores/:managerId');
+  const managerId = req.params.managerId;
+  const query = `SELECT CONCAT(first_name, ' ', last_name) as full_name, employee_id FROM hr_v2.employees WHERE manager_id = ?`;
 
-    connection.query(query, [managerId], (err, result) => {
-      if (err) {
-        console.error('Error retrieving employees:', err);
-        res.status(500).json({ message: 'Error retrieving employees' });
+  connection.query(query, [managerId], (err, result) => {
+    if (err) {
+      console.error('Error retrieving employees:', err);
+      res.status(500).json({ message: 'Error retrieving employees' });
+    } else {
+      if (result.length === 0) {
+        // No se encontraron trabajadores para el manager específico o el manager no existe
+        res.status(404).json({ message: 'No se encuentran trabajadores para ese manager o el manager no existe.' });
       } else {
         console.log('Successful search for employees with manager_id:', managerId);
-        res.json(result);
+
+        // Crear un arreglo con objetos que contienen employee_id y la concatenación de nombres
+        const employeesData = result.map((employee) => {
+          return {
+            employee_id: employee.employee_id,
+            full_name: employee.full_name
+          };
+        });
+
+        // Enviar el arreglo como respuesta
+        res.json(employeesData);
       }
-    });
+    }
   });
+});
+
+app.get('/buscar/:employeeId', (req, res) => {
+  console.log('Receiving a request at /buscar/:employeeId');
+  const employeeId = req.params.employeeId;
+  const query = `
+    SELECT e.employee_id, CONCAT(e.first_name, ' ', e.last_name) as full_name, e.email, e.phone_number,
+           e.hire_date, j.job_title, e.salary, 
+           IFNULL(e.commission_pct, '-') as commission_pct,
+           e.manager_id,
+           IFNULL(d.department_name, '-') as department_name,
+           IFNULL(e.meeting, '-') as meeting,
+           IFNULL(e.meeting_date, '-') as meeting_date,
+           IFNULL(e.employee_feedback, '-') as employee_feedback
+    FROM hr_v2.employees e
+    LEFT JOIN jobs j ON e.job_id = j.job_id
+    LEFT JOIN departments d ON e.department_id = d.department_id
+    WHERE e.employee_id = ?`;
+  
+  connection.query(query, [employeeId], (err, result) => {
+    if (err) {
+      console.error('Error searching for employee:', err);
+      res.status(500).json({ message: 'Error searching for employee' });
+    } else {
+      if (result.length === 0) {
+        // No se encontró un empleado con ese employee_id
+        res.status(404).json({ message: 'No se encontró un empleado con ese employee_id.' });
+      } else {
+        console.log('Successful search for employee with employee_id:', employeeId);
+
+        // Enviar la respuesta en formato JSON
+        res.json(result[0]);
+      }
+    }
+  });
+});
 
 connection.connect((err) => {
   if (err) {
